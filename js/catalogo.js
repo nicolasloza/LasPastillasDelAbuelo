cf1a89e516e786c5f80b1b5b834147eb83af444e
nicolasloza <nicolaslozaspataro@gmail.com>
2022-08-02 16:43:57 -0300

    Agrega imagenes en catalogo y buscador

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,19 +1,102 @@
+let productos = [
+    {
+        "id": 1,
+        "nombre": "2020",
+        "img":"../multimedia/images/catalogo/products/2020.jpg",
+        "precio": 1300,
+        "añoLanzamiento": "2020", 
+        "cantidad": 1
+        
+    },
+    {
+        "id": 2,
+        "nombre": "Locura y Realidad",
+        "img": "../multimedia/images/catalogo/products/locura-y-realidad.jpg",
+        "precio": 2300,
+        "añoLanzamiento": "2017", 
+        "cantidad": 1
+    },
+    {
+        "id": 3,
+        "nombre": "Paradojas",
+        "img": "../multimedia/images/catalogo/products/paradojas.jpg",
+        "precio": 1700,
+        "añoLanzamiento": "2015", 
+        "cantidad": 1
+    },
+    {
+        "id": 4,
+        "nombre": "El Barrio en sus Puños",
+        "img": "../multimedia/images/catalogo/products/elbarrio.jpg",
+        "precio": 1400,
+        "añoLanzamiento": "2014", 
+        "cantidad": 1
+    },
+    {
+        "id": 5,
+        "nombre": "10 años",
+        "img": "../multimedia/images/catalogo/products/10.jpg",
+        "precio": 1250,
+        "añoLanzamiento": "2013                                                                                                                                                             ", 
+        "cantidad": 1
+    },
+    {
+        "id": 6,
+        "nombre": "Kermesse",
+        "img": "../multimedia/images/catalogo/products/Kermesse.jpg",
+        "precio": 1200,
+        "añoLanzamiento": "2012", 
+        "cantidad": 1
+    },
+    {
+        "id": 7,
+        "nombre": "Desafios",
+        "img": "../multimedia/images/catalogo/products/desafios.jpg",
+        "precio": 1400,
+        "añoLanzamiento": "2011", 
+        "cantidad": 1
+    },
+    {
+        "id": 8,
+        "nombre": "Versiones",
+        "img": "../multimedia/images/catalogo/products/versiones.jpg",
+        "precio": 2500,
+        "añoLanzamiento": "2010", 
+        "cantidad": 1
+    },
+    {
+        "id": 9,
+        "nombre": "Crisis",
+        "img": "../multimedia/images/catalogo/products/crisis.jpg",
+        "precio": 1450,
+        "añoLanzamiento": "2008", 
+        "cantidad": 1
+    },
+    {
+        "id": 10,
+        "nombre": "Las Pastillas del Abuelo",
+        "img": "../multimedia/images/catalogo/products/discorojo.jpg",
+        "precio": 1350,
+        "añoLanzamiento": "2006", 
+        "cantidad": 1
+    },
+    {
+        "id": 11,
+        "nombre": "Por Colectora",
+        "img": "../multimedia/images/catalogo/products/por-colectora.jpg",
+        "precio": 1600,
+        "añoLanzamiento": "2005", 
+        "cantidad": 1
+    }
+]
+
+
 const resultados = document.getElementById('resultados');
 
-/* fetchProductos()
+const ordenPrecio = document.getElementById('orden-precio')
+const ordenLanzamiento = document.getElementById('orden-lanzamiento')
+const ordenTitulo = document.getElementById('orden-titulo')
 
-function fetchProductos () {
-    return fetch('https://mocki.io/v1/711f99d3-ed17-4a04-a0b9-f1af5785abb6')
-    .then((res) => {
-        const response = res.json();
-        return response;
-    })
-    .then((resultado) => {
-        productos = resultado;
-        renderizarResultados(resultado);
-    })
-    .catch(err => console.log('No se pudo cargar la información sobre los productos'));
-} */
 
 const renderizarResultados = () => {
     
@@ -21,7 +104,7 @@ const renderizarResultados = () => {
         
         let resultadoHTML = `
             <tr>
-                <td> ${res.img}</td>
+                <td> <img class="img-disco" src="${res.img}"></td>
                 <td class="tabla-color"> ${res.nombre}</td>
                 <td class="tabla-color"> ${res.añoLanzamiento}</td>
                 <td class="tabla-color">$${res.precio}</td>
@@ -39,7 +122,7 @@ const actualizarResultados = () => {
         
         let resultadoHTML = `
             <tr>
-                <td> ${res.img}</td>
+                <td> <img class="img-disco" src="${res.img}"></td>
                 <td class="tabla-color"> ${res.nombre}</td>
                 <td class="tabla-color"> ${res.añoLanzamiento}</td>
                 <td class="tabla-color">$${res.precio}</td>
@@ -47,11 +130,27 @@ const actualizarResultados = () => {
         ` 
         resultados.innerHTML = resultadoHTML
     })
-    console.log('3',productos)
-
 }
 
+function busqueda() {
+    let buscador = document.getElementById('buscador').value.toLowerCase()
 
+    for (let i = 0; i < resultados.rows.length; i++) {
+        let celdas = resultados.rows[i].getElementsByTagName('td')
+        let found = false
+        for (let e = 0; e < celdas.length && !found; e++) {
+            let comparar = celdas [e].innerHTML.toLowerCase()
+            if(buscador.length == 0 || (comparar.indexOf(buscador) > -1)) {
+                found = true
+            }
+        }
+        if (found) {
+            resultados.rows[i].style.display = ''
+        } else {
+            resultados.rows[i].style.display = 'none'
+        }
+    }
+}
 
 
 /* function buscadorInput() {
@@ -83,22 +182,17 @@ const actualizarResultados = () => {
     })
 } */
 
-const ordenPrecio = document.getElementById('orden-precio')
-const ordenLanzamiento = document.getElementById('orden-lanzamiento')
-const ordenTitulo = document.getElementById('orden-titulo')
+
 
 ordenPrecio.addEventListener('click', () => {
-    // productos.sort((a,b) => {
-
-    //     if (a.precio < b.precio) {
-    //         return -1
-    //     }
-    
-    //     if (a.precio > b.precio) {
-    //         return 1
-    //     }
-    //     return 0
-    // })
+    productos.sort((a,b) => {
+        if (a.precio < b.precio) {
+            return -1
+        } else if (a.precio > b.precio) {
+            return 1
+        }      
+        return 0
+    })
 
     productos.sort((a, b) => {
         if (a.precio === b.precio) {
@@ -110,9 +204,10 @@ ordenPrecio.addEventListener('click', () => {
         }
         return 1
     })
-    console.log('1',productos)
+
+
     actualizarResultados()
-    console.log('2',productos)
+
 })
 
 ordenLanzamiento.addEventListener('click', () => {
@@ -120,12 +215,14 @@ ordenLanzamiento.addEventListener('click', () => {
 
         if (a.añoLanzamiento < b.añoLanzamiento) {
             return -1
+        } else if (a.añoLanzamiento > b.añoLanzamiento) {
+            return 1
+        } else {
+            return 0
         }
     
-        if (a.añoLanzamiento > b.añoLanzamiento) {
-            return 1
-        }
-        return 0
+        
+        
     })
 
     // productos.sort((a, b) => {
@@ -139,7 +236,7 @@ ordenLanzamiento.addEventListener('click', () => {
     //     return 1
     // })
 
-    console.log(productos)
+    actualizarResultados()
 })
 
 ordenTitulo.addEventListener('click', () => {
@@ -147,17 +244,16 @@ ordenTitulo.addEventListener('click', () => {
 
         if (a.nombre == b.nombre) {
             return 0;
-        }
-        if (a.nombre < b.nombre) {
+        } else if (a.nombre < b.nombre) {
             return -1;
+        } else {
+            return 1;
         }
-        return 1;
     })
 
-    console.log(productos)
+    actualizarResultados()
 
 })
-
 
 /* productos.sort((a,b) => {
     
